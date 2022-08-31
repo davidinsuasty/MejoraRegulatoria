d014979059bfb3a64bdee57c6053a062cdae3b1d
David Insuasti <davidinsuasty@gmail.com>
2022-08-31 13:37:48 -0500

    Revert "Cambios menores"
    This reverts commit 31c87f86645bf20290d23906b68ed9a74f3e30b6.

--- 00_leer_rtf.py
+++ 00_leer_rtf.py
@@ -12,14 +12,13 @@ import glob
 import parmap
 
 
-path = './../../Insumos/DB_1991-2014_full'
-path_out = './../../Insumos/txt_planos_91-2014/'
-all_docs = glob.glob(path + '/*/*/*.rtf')
-
+path = './../../Insumos/DB1991_2014_full'
+path_out = './../../Insumos/txt_planos_91-98_/'
+all_docs = glob.glob(path + '/*/*.rtf')
 n = len(all_docs)
-if __name__ == '__main__':
-    texto = parmap.map(
-        lectura.leer_texto, all_docs, 'rtf', 'false', '', 'True', pm_pbar=True, pm_processes=12)
-    for i, file in enumerate(all_docs):
-        escribir_texto(path_out + file.split('\\')
-                       [-1].split('.')[0].upper() + '.txt', texto[i], tipo='txt')
+if __name__=='__main__':
+    texto = parmap.map(lectura.leer_texto,all_docs,'rtf','false','','True',pm_pbar=True, pm_processes=8)
+    for i,file in enumerate(all_docs):
+        escribir_texto(path_out + file.split('\\')[-1].split('.')[0].upper() + '.txt' ,texto[i],tipo='txt')
+    
+    
